96bb8f74c1c1af7752f1c0721eac27e99adfd328
kwanghyun.won <kwanghyun.won@jameasy.com>
2017-12-20 16:14:14 +0900

    remove storyboard

--- contacts/Contacts/Contacts/AppDelegate.swift
+++ contacts/Contacts/Contacts/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+
         return true
     }
 
--- contacts/Contacts/Contacts/ViewController.swift
+++ contacts/Contacts/Contacts/ViewController.swift
@@ -2,19 +2,15 @@
 //  ViewController.swift
 //  Contacts
 //
-//  Created by Jameasy on 2017. 12. 20..
+//  Created by kwanghyun.won. on 2017. 12. 20..
 //  Copyright © 2017년 kwanghyun.won. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 }
