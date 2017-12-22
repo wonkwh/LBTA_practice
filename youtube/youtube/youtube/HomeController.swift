2efb4e1fc5c8c24de397ff40772584279973f968
wonkwh <wonkwh@gmail.com>
2017-12-22 20:21:46 +0900

    create youtube project & collection view

---
+++ youtube/youtube/youtube/HomeController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Jameasy on 2017. 12. 22..
+//  Copyright © 2017년 kwanghyun.won. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = .white
+        collectionView?.register(UICollectionViewCell.self , forCellWithReuseIdentifier: "CellID")
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellID", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+
