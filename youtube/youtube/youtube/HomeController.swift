f65ef87fd289abb4902cd353ce6db6250036df0e
wonkwh <wonkwh@gmail.com>
2017-12-22 20:44:07 +0900

    add CollectionViewCell

--- youtube/youtube/youtube/HomeController.swift
+++ youtube/youtube/youtube/HomeController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  youtube
 //
-//  Created by Jameasy on 2017. 12. 22..
+//  Created by kwanghyun.won on 2017. 12. 22..
 //  Copyright © 2017년 kwanghyun.won. All rights reserved.
 //
 
@@ -15,7 +15,7 @@ class HomeController: UICollectionViewController {
 
         navigationItem.title = "Home"
         collectionView?.backgroundColor = .white
-        collectionView?.register(UICollectionViewCell.self , forCellWithReuseIdentifier: "CellID")
+        collectionView?.register(VideoCell.self , forCellWithReuseIdentifier: VideoCell.cellID)
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -23,8 +23,7 @@ class HomeController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellID", for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: VideoCell.cellID, for: indexPath)
         return cell
     }
 }
@@ -32,5 +31,53 @@ class HomeController: UICollectionViewController {
 extension HomeController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
+
+class VideoCell: UICollectionViewCell {
+    static let cellID = "videoCellID"
+
+    let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+        constraintViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addViews() {
+        addSubview(thumbnailImageView)
+        addSubview(separatorView)
+    }
+
+    func constraintViews() {
+
+        thumbnailImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
+        thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
+        thumbnailImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        thumbnailImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
+
+        separatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        separatorView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1).isActive = true
+        separatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
 }
