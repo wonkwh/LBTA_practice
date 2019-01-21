789e2112ae517059d2e8d917bb4978c411718ef2
kwanghyun.won <wonkwh@gmail.com>
2019-01-21 11:00:29 +0900

    draw line with CGContext

--- DrawPad/DrawPad/ViewController.swift
+++ DrawPad/DrawPad/ViewController.swift
@@ -8,13 +8,71 @@
 
 import UIKit
 
+class CanvasView: UIView {
+    
+    var lines = [[CGPoint]]()
+    
+    override func draw(_ rect: CGRect) {
+        //custom drawing
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 100, y: 100)
+//
+//        context.move(to: startPoint)
+//        context.addLine(to: endPoint)
+    
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.setLineCap(.round)
+        
+        
+        lines.forEach { (line) in
+            for (index, point) in line.enumerated() {
+                if index == 0 {
+                    context.move(to: point)
+                } else {
+                    context.addLine(to: point)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    //track the finger
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else { return }
+            
+        guard var lastLine = lines.popLast() else { return }
+    
+        lastLine.append(point)
+        lines.append(lastLine)
+            //lines.append(point)
+            setNeedsDisplay()
+        
+    }
+}
+
+
 class ViewController: UIViewController {
 
+    let canvasView = CanvasView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(canvasView)
+        canvasView.backgroundColor = .white
+        canvasView.frame = view.frame
     }
-
-
 }
 
