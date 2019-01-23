421f7f2fd4169e54f518431d1f1ccf64b5772ebc
kwanghyun.won <wonkwh@gmail.com>
2019-01-23 17:24:54 +0900

    add headerView in UICollectionViewController

---
+++ StretchyHeaderCollectionView/StretchyHeaderCollectionView/CollectionHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionHeaderView.swift
+//  StretchyHeaderCollectionView
+//
+//  Created by wonkwh on 23/01/2019.
+//  Copyright © 2019 wonkwh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- StretchyHeaderCollectionView/StretchyHeaderCollectionView/StretchyHeaderController.swift
+++ StretchyHeaderCollectionView/StretchyHeaderCollectionView/StretchyHeaderController.swift
@@ -8,27 +8,37 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
 
 class StretchyHeaderController: UICollectionViewController {
 
+    fileprivate let cellId = "CellId"
+    fileprivate let headerId = "headerId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
         
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(CollectionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: headerId)
         // Do any additional setup after loading the view.
     }
 
     // MARK: - UICollectionViewDataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        return header
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 35
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
     
         // Configure the cell
         cell.backgroundColor = .black
@@ -36,9 +46,14 @@ class StretchyHeaderController: UICollectionViewController {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension StretchyHeaderController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: collectionView.frame.width, height: 40)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: view.frame.width, height: 200)
     }
 }
 
